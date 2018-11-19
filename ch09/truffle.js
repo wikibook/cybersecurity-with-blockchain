bac268b4cb270e6eca8403e7e1ad6b0a2e23361d
위키북스 <dylee@wikibook.co.kr>
2018-11-19 15:24:53 +0900

    First commit

---
+++ ch09/truffle.js
@@ -0,0 +1,29 @@
+let HDWalletProvider = require('truffle-hdwallet-provider')
+
+module.exports = {
+    networks: {
+        development: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*" // Match any network id
+        },
+        truffle: {
+            host: "localhost",
+            port: 9545,
+            network_id: "*", // Match any network id
+        },
+        travisci: {
+            host: "localhost",
+            port: 8545,
+            network_id: "*"
+        },
+        rinkeby: {
+            host: "localhost", // Connect to geth on the specified
+            port: 8545,
+            from: "0x0085f8e72391Ce4BB5ce47541C846d059399fA6c",
+            // default address to use for any transaction Truffle makes during migrations
+            network_id: 4,
+            gas: 4612388 // Gas limit used for deploys
+        }
+    }
+};
